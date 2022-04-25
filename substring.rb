6ebe0c7cde763da917ed3aa68a6117ec13fe8335
Guido Ferri <guidoferris@gmail.com>
2022-04-25 16:49:00 +0200

    Added substring exercize

---
+++ substring.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def substrings(word, valid_substrings)
+  downcased_word = word.downcase
+  valid_substrings.each_with_object({}) do |substring, hash|
+    count = downcased_word.scan(substring).length
+    hash[substring] = count if count.positive?
+  end
+end
+
+dictionary = %w[
+  below down go going horn how howdy it i low own part partner sit
+  below down go going horn how howdy it i low own part partner sit
+]
+p substrings('below', dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
